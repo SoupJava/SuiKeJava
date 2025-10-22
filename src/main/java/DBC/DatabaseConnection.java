c07979bd83845fa6ed6288c76d317e6fad2be646
紫菜蛋花汤 <tcg_mail@gs.zzu.edu.cn>
2025-10-22 22:00:01 +0800

    Update DatabaseConnection.java

--- src/main/java/DBC/DatabaseConnection.java
+++ src/main/java/DBC/DatabaseConnection.java
@@ -12,9 +12,9 @@ public class DatabaseConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             // conn = DriverManager.getConnection(url);
             conn = DriverManager.getConnection(
-                    "jdbc:mysql://rm-uf6r45v7ev7nw50chao.mysql.rds.aliyuncs.com:3306/skwork?serverTimezone=UTC",
-                    "tt123",
-                    "Tt3837120");
+                    "数据库地址",
+                    "用户名",
+                    "密码");
         } catch (Exception e) {
             String Text = "请检查是否连接网络或者防火墙设置";
             JOptionPane.showMessageDialog(null, Text, "警告", JOptionPane.WARNING_MESSAGE);
@@ -35,4 +35,5 @@ public class DatabaseConnection {
             }
         }
     }
+
 }
